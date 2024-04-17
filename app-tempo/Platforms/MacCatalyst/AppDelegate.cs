9c76a6c0f67f37b31cfb6331e135bc0989a36104
apaixonadopormotores <pedrofernandes0124@gmail.com>
2024-04-17 08:11:03 -0300

    butao do pedros

---
+++ app-tempo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace app_tempo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
