d3664d4767e9ecf62a9484b241ab6ea4b35768ee
apaixonadopormotores <pedrofernandes0124@gmail.com>
2024-04-18 11:40:08 -0300

    a fds

---
+++ app-tempo/results.cs
@@ -0,0 +1,11 @@
+public class results
+{
+public int temp {get;set;}
+public int date {get;set;}
+public int time {get;set;}
+public string city {get;set;}
+public string humidity {get;set;}
+public int cloudness {get;set;}
+public int rain {get;set;}
+public int windspeed {get;set;}
+}
